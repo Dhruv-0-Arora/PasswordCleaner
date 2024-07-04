f421d0dec30f699d49acae3ec160eb9212b0a254
Dhruv Arora <dhruv.arora1@autodesk.com>
2024-07-03 21:21:11 -0700

    Adding theoretical code that should work if cargo run is ran with proper csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, error::Error, process};
+
+#[derive(Debug, serde::Deserialize)]
+struct Entry {
+    folder: String,
+    favorite: bool,
+    type_: String,
+    name: String,
+    notes: String,
+    fields: String,
+    reprompt: String,
+    login_url: String,
+    login_username: String,
+    login_password: String,
+    login_totp: String,
 }
+
+fn fetch_entries() -> Result<(), Box<dyn Error>> { 
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+//fn write_file() {
+//
+//}
+
+fn main() {
+    println!("Please ensure you are in the same directory as your password csv file. Please then input the name of your csv file ending with '*.csv'");
+    let mut file_name = String::new();
+    let _ = io::stdin().read_line(&mut file_name);
+    if let Err(err) = fetch_entries(file_name) {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
+}
+
